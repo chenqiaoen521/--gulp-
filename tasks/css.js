d10a2e146519b19d722d3069dcb95c00f433ee42
chenqiaoen521 <274078815@qq.com>
2018-01-07 10:42:24 +0800

    add

--- tasks/css.js
+++ tasks/css.js
@@ -4,11 +4,14 @@ import rename from 'gulp-rename';
 import minifycss from 'gulp-minify-css';
 import args from './util/args';
 import livereload from 'gulp-livereload';
+import cleanCss from 'gulp-clean-css';
+import concat from 'gulp-concat';
 
 gulp.task('css', ()=>{
-  return gulp.src('app/css/*.css')
+  return gulp.src(['app/css/reset.css','app/css/layout.css','app/css/lottery.css'])
+  .pipe(concat('main.css'))
+  .pipe(cleanCss())
   .pipe(rename({ suffix: '.min' }))
-  .pipe(minifycss())
   .pipe(gulp.dest('server/public/css'))
   .pipe(gulpif(args.watch, livereload()));
 })
